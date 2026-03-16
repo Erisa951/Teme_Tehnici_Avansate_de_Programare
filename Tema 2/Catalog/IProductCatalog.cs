794b50328e1ff2f538d26dedd6a12a807d5b71a3
erisaandreea <erisaandreea@gmail.com>
2026-03-16 02:18:47 +0200

    Adaug Tema2_proiect_nou în repository

---
+++ Tema 2/Catalog/IProductCatalog.cs
@@ -0,0 +1,14 @@
+﻿using Tema_2.Domain;
+
+namespace Tema_2.Catalog;
+
+public interface IProductCatalog
+{
+    void Add(Product product);
+
+    IReadOnlyList<Product> GetAll();
+
+    IEnumerable<Product> Search(ProductFilter filter);
+
+    IEnumerable<IGrouping<string, Product>> GroupByCategory();
+}
---
+++ Tema 2/Catalog/ProductFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace Tema_2.Catalog;
+
+public class ProductFilter
+{
+    public string? Category { get; set; }
+
+    public decimal? MinPrice { get; set; }
+
+    public decimal? MaxPrice { get; set; }
+
+    public ProductOrderBy? OrderBy { get; set; }
+
+    public bool Descending { get; set; }
+}
+
+public enum ProductOrderBy
+{
+    Name,
+    Price
+}
